5db755c69cc996fd414a88dfe14d14981e08ec78
Aryan Kakadia <108693136+Flashy990@users.noreply.github.com>
2023-08-12 16:29:57 -0400

    Add files via upload

---
+++ ReactionTimeTestingGame/Assets/PlaneColorChanger.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneColorChanger : MonoBehaviour
+{
+
+    float roundStartTime;
+    float waitTime;
+    bool startedFlag = false;
+    Renderer myPlaneRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPlaneRenderer = GetComponent<Renderer>();
+        changeColorToRed();
+        print("Press Enter to start.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && startedFlag == false)
+        {
+            startRoutine();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && startedFlag == true)
+        {
+            pressSpaceRoutine();
+        }
+        /*
+        else {
+
+            print("Please press enter to start.");
+        
+        }
+        */
+    }
+
+    void SetRandomTime()
+    {
+        waitTime = Random.Range(2, 11);
+        roundStartTime = Time.time;
+        print(waitTime + " seconds.");
+    }
+
+    void changeColorToGreen()
+    {
+        myPlaneRenderer.material.color = Color.green;
+    }
+
+    void changeColorToRed()
+    {
+        myPlaneRenderer.material.color = Color.red;
+    }
+
+    void changeColorToBlue()
+    {
+        myPlaneRenderer.material.color = Color.blue;
+    }
+
+    void startRoutine()
+    {
+        print("Starting game.");
+        changeColorToRed();
+        SetRandomTime();
+        startedFlag = true;
+        print("Hit Spacebar when the color changes to green.");
+        Invoke("changeColorToGreen", waitTime);
+    }
+
+    void pressSpaceRoutine()
+    {
+        float reactionTimeTaken = Time.time - roundStartTime - waitTime;
+        if (reactionTimeTaken < 0)
+        {
+            CancelInvoke("changeColorToGreen");
+            print("Pressed too early");
+            startedFlag = false;
+            changeColorToBlue();
+            print("Press Enter to go again.");
+        }
+        else
+        {
+            CancelInvoke("changeColorToGreen");
+            print("Reaction time: " + reactionTimeTaken);
+            startedFlag = false;
+            changeColorToRed();
+            print("Press Enter to go again.");
+        }
+    }
+
+}
---
+++ ReactionTimeTestingGame/README.md
@@ -0,0 +1,7 @@
+# Reaction Testing Game
+This is my first Unity project. Its a simple game to test your reaction time. Press "Enter" to start and hit space when the screen turns green to see how fast you react.
+
+## TODO
+- Add a UI to show the player reaction time on top left.
+- Add play again screen.
+- Add a database of player tries and graph the reaction times to see imporvements or changes over time.
